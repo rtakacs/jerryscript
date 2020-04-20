04a57b0517d31abfb48f55863b41771d12a87538
Roland Takacs <rtakacs@inf.u-szeged.hu>
2020-04-20 11:11:10 +0200

    Modify the algorithm of the hashmap 4

--- jerry-core/ecma/base/ecma-property-hashmap.h
+++ jerry-core/ecma/base/ecma-property-hashmap.h
@@ -78,8 +78,8 @@ void ecma_property_hashmap_insert (ecma_property_header_t *property_header_p,
 ecma_property_hashmap_delete_status ecma_property_hashmap_delete (ecma_property_header_t *property_header_p,
                                                                   ecma_property_t *property_p);
 
-ecma_property_t *ecma_property_hashmap_find (ecma_property_header_t *property_header_p, ecma_string_t *name_p,
-                                             jmem_cpointer_t *property_real_name_cp, ecma_property_index_t *index);
+ecma_property_t *ecma_property_hashmap_find (ecma_object_t *obj_p, ecma_property_header_t *property_header_p,
+                                             ecma_string_t *name_p);
 #endif /* ENABLED (JERRY_PROPRETY_HASHMAP) */
 
 /**
